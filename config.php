0b7d7d618d35faba1475f5f731b7c7db070493e7
asya1122 <63053184+asya1122@users.noreply.github.com>
2021-08-16 23:46:50 +0300

    sanayi-pazar adım 2

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+$currency = '$'; //Currency sumbol or code
+
+//db settings
+$db_username = 'root';
+$db_password = 'cicek.21';
+$db_name = 'offers';
+$db_host = 'localhost';
+$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+
+
+?>
